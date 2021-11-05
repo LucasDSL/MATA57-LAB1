f9ca0a6e35146f5f2d1a5bda63dec82a4b7d2286
adrielle <93674291+adrielle19@users.noreply.github.com>
2021-11-05 15:04:56 -0300

    Update e7.c

--- 02 Sub-Rotinas 2/e7.c
+++ 02 Sub-Rotinas 2/e7.c
@@ -1,3 +1,4 @@
+//feito por Adrielle Andrade Carvalho
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -15,25 +16,25 @@ int main() {
 	printf("Informe o quarto numero:\n");
 	scanf("%d", &n4);
 	if((primo(n1)==0) && (primo(n2)==0) && (primo(n3)==0) && (primo(n4)==0)){
-		//se todos forem primos o produto será calculado
+		//se todos forem primos o produto serÃ¡ calculado
 		printf("Produto: %d", produto(n1, n2, n3, n4));
 	}else{
-		//caso contrário, esta mensagem será impressa
+		//caso contrÃ¡rio, esta mensagem serÃ¡ impressa
 		printf("A operacao nao pode ser executada\n");
 	}
     return 0;
 }
 
-int primo(int n){// função que verifica se os numeros são primos
+int primo(int n){// funÃ§Ã£o que verifica se os numeros sÃ£o primos
 	int i;
-	//para ser primo, o numero só pode ser dividido por si mesmo e por um
-	//para fazer a verificação, observamos se o numero é dividido por qualquer x>=2 e menor ou igual a sua metade
+	//para ser primo, o numero sÃ³ pode ser dividido por si mesmo e por um
+	//para fazer a verificaÃ§Ã£o, observamos se o numero Ã© dividido por qualquer x>=2 e menor ou igual a sua metade
 	for(i=2;i<=n/2;i++){
-		if(n%i==0){// se encontrarmos um divisor, a função retorna 1 
+		if(n%i==0){// se encontrarmos um divisor, a funÃ§Ã£o retorna 1 
 			return 1;
 		}
 	}
-	return 0;// se não encontrarmos, o numero sera primo e a função retorna 0
+	return 0;// se nÃ£o encontrarmos, o numero sera primo e a funÃ§Ã£o retorna 0
 }
 
 int produto(int n1, int n2, int n3, int n4){//funcao que retorna o produto
