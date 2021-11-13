801ff572ddd8df1901c38af26a34b9473bc9f626
fosterguys <62483900+igordantasgf@users.noreply.github.com>
2021-11-13 14:05:29 -0300

    Update alocacao13.c

--- 08 Alocação Dinâmica/alocacao13.c
+++ 08 Alocação Dinâmica/alocacao13.c
@@ -1,6 +1,5 @@
-// Escreva um programa que aloque dinamicamente uma matriz (de inteiros) de dimensões definidas pelo usuário e a leia.
-// Em seguida, implemente uma função que receba um valor e: retorne 1, caso o valor esteja na matriz; 
-// ou retorne 0, caso não esteja na matriz.
+// feito por Igor Dantas
+// https://github.com/igordantasgf
 
 #include <stdio.h>
 #include <stdlib.h>
