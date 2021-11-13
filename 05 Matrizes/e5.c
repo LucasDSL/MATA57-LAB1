142b1a1195693bbd367322f05c5d4f76684a0117
adrielle <93674291+adrielle19@users.noreply.github.com>
2021-11-13 20:01:02 -0300

    modularização da e5

--- 05 Matrizes/e5.c
+++ 05 Matrizes/e5.c
@@ -1,28 +1,35 @@
+//feito por Adrielle Andrade Carvalho
 #include <stdio.h>
+int m, n;
 
-int main(){
-	int m, n, i, j, v, g, p1, p2, h, k, l;
-	scanf("%d", &m);
-	scanf("%d", &n);
-	scanf("%d", &v);
-	scanf("%d", &g);
-	int campo[m+2][n+2], campoaux[m+2][n+2];
-	for(i=0;i<m+2;i++){
-		for(j=0;j<n+2;j++){//preenche o campo e o auxiliar com 0
+void dados(int campo[m][n], int v){
+	int i, j, p1, p2;
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){//preenche o campo com 0
 			campo[i][j]=0;
-			campoaux[i][j]=0;
 		}
 	}
 	for(i=0;i<v;i++){//coloca 1 nas posições das células vivas
 		scanf("%d", &p1);
 		scanf("%d", &p2);
 		campo[p1+1][p2+1]=1;
-		campoaux[p1+1][p2+1]=1;
 	}
-	int redor=0;
-	for(h=0;h<g;h++){//loop para as gerações
-		for(i=1;i<=m;i++){
-			for(j=1;j<=n;j++){//comparação com as 8 posições ao redor da posição, acumulando +1 cada vez que tiver uma célula viva
+}
+
+void caux(int campo[m][n], int campoaux[m][n]){//a matriz auxiliar é uma cópia da original para receber as atualizações
+	int i, j;
+	for(i=0;i<m;i++){
+		for(j=0;j<n;j++){//preenche o campo auxiliar 
+			campoaux[i][j] = campo[i][j];
+		}
+	}
+}
+
+void jogo(int campo[m][n], int campoaux[m][n]){
+	int i, j;
+	int redor = 0;
+	for(i=1;i<m;i++){
+			for(j=1;j<n;j++){//comparação com as 8 posições ao redor da posição, acumulando +1 cada vez que tiver uma célula viva
 				if(campo[i-1][j-1]==1){
 					redor++;
 				}
@@ -57,14 +64,29 @@ int main(){
 				redor=0;
 			}
 		}
-		for(i=1;i<=m;i++){//campo é atualizado e impresso depois de ser totalmente analisado
-			for(j=1;j<=n;j++){
+		for(i=1;i<m;i++){//campo é atualizado e impresso depois de ser totalmente analisado
+			for(j=1;j<n;j++){
 				campo[i][j]=campoaux[i][j];
 				printf("%d ", campo[i][j]);
 			}
 			printf("\n");
 		}
 		printf("\n");
+}
+
+int main(){
+	int i, j, v, g;
+	scanf("%d", &m);
+	scanf("%d", &n);
+	scanf("%d", &v);
+	scanf("%d", &g);
+	m = m + 2;
+	n = n + 2;
+	int campo[m][n], campoaux[m][n];
+	dados(campo, v);
+	caux(campo, campoaux);
+	for(i=0;i<g;i++){
+		jogo(campo, campoaux);
 	}
 	return 0;
 }
