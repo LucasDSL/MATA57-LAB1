ff470e418f32376fca9faced262105abcf37d9da
fosterguys <62483900+igordantasgf@users.noreply.github.com>
2021-11-13 14:06:11 -0300

    Update and rename alocacao7.c to e7.c

--- 08 Alocação Dinâmica/e7.c
+++ 08 Alocação Dinâmica/e7.c
@@ -1,7 +1,5 @@
-//Exercício 07. Escreva um programa que leia primeiro os 6 números gerados pela loteria e depois os 6 números do seu bilhete. 
-//O programa então verifica quantos números o jogador acertou. 
-//Em seguida, ele aloca espaço para um vetor de tamanho igual a quantidade de números corretos e guarda os números corretos nesse vetor. 
-//Finalmente, o programa exibe os números sorteados e os seus números corretos.
+// feito por Igor Dantas
+// https://github.com/igordantasgf
 
 #include <stdio.h>
 #include <stdlib.h>
