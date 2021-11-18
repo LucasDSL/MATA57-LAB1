df3daaa0c487a8c1a9b75014dae7d2d7dc347f68
Iasmim Marinho <72471500+IasmimMarinho@users.noreply.github.com>
2021-11-18 16:48:35 -0300

    Add files via upload

---
+++ 04 Recursivas 2/e1.c
@@ -0,0 +1,29 @@
+//dada a quantidade de blocos existentes na base, retorne o total de blocos da pirâmide.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int 
+totalBlocos(int base);
+
+int main(void) {
+  int base, total;
+
+  printf("Quantos blocos existem na base da piramide?\n");
+  scanf("%d", &base);
+  //chama a função
+  total = totalBlocos(base);
+
+  printf("Total = %d blocos!\n", total);
+}
+
+int 
+totalBlocos(int base){
+//caso base: quando número de blocos da base for 1
+if(base == 1){
+  return base;
+}
+else
+//faz chamada recursiva subtraindo 1 bloco e somando com a base
+  return base + totalBlocos(base - 1) ;
+}
